ee99a5c4a24cb416f11af146d9d6f2afa1a075e9
Shahrukh Raza <shahrukh4062@gmail.com>
2024-04-19 17:03:32 +0900

    raza

--- src/requirements/mariadb/tools/setup_db.sh
+++ src/requirements/mariadb/tools/setup_db.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
 
 # Wait for MariaDB server to start
-while ! mysqladmin ping --silent --socket=/tmp/mysqld.sock; do
-    sleep 1
+while ! mysqladmin ping --silent --socket=/var/run/mysqld/mysqld.sock ; do
+  echo "Waiting for database connection..."
+  sleep 5
 done
 
-# execute mysql_secure_installation script
-expect -c '
-set timeout 5;
-spawn mysql_secure_installation --socket=/tmp/mysqld.sock;
-expect "Enter current password for root (enter for none): ";
-send -- "\n";
-expect "Set root password?";
-send -- "n\n";
-expect "Remove anonymous users?";
-send "Y\n";
-expect "Disallow root login remotely?";
-send "N\n";
-expect "Remove test database and access to it?";
-send "Y\n";
-expect "Reload privilege tables now?";
-send "Y\n";
-interact;'
-echo "mysql_secure_installation finished:$?"
-
 # データベースがなかったら作成する→基本的な設定を行うスクリプト（パスワードやルート権限等）
-mysql --socket=/tmp/mysqld.sock -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB_NAME}\`;"
-mysql --socket=/tmp/mysqld.sock -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER_NAME}\`@'localhost' IDENTIFIED BY '${MYSQL_USER_PW}';"
-mysql --socket=/tmp/mysqld.sock -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB_NAME}\`.* TO \`${MYSQL_USER_NAME}\`@'%';"
-mysql --socket=/tmp/mysqld.sock -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PW}';"
-mysql --socket=/tmp/mysqld.sock -e "FLUSH PRIVILEGES;"
+mysql --socket=/var/run/mysqld/mysqld.sock -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB_NAME}\`;"
+mysql --socket=/var/run/mysqld/mysqld.sock -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER_NAME}\`@'localhost' IDENTIFIED BY '${MYSQL_USER_PW}';"
+mysql --socket=/var/run/mysqld/mysqld.sock -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB_NAME}\`.* TO \`${MYSQL_USER_NAME}\`@'%';"
+mysql --socket=/var/run/mysqld/mysqld.sock -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PW}';"
+mysql --socket=/var/run/mysqld/mysqld.sock -e "FLUSH PRIVILEGES;"
 
 # mysqladmin -u root -p $MYSQL_ROOT_PW shutdown
