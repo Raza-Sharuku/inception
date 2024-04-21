bf700da17efa10ba1cb976a75286d916f4022056
Shahrukh Raza <shahrukh4062@gmail.com>
2024-04-21 15:44:33 +0900

    mariadb done

--- src/requirements/mariadb/tools/setup_db.sh
+++ src/requirements/mariadb/tools/setup_db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # MariaDBサーバーを起動する関数
 start_mariadb() {
   mysqld --defaults-file=/etc/mysql/mariadb.conf.d/50-server.cnf --innodb_use_native_aio=0
@@ -17,11 +18,15 @@ setup_mariadb() {
   # データベースがなかったら作成する→基本的な設定を行うスクリプト（パスワードやルート権限等）
   mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB_NAME}\`;"
   
-  # rootユーザーのパスワードを設定
+  # rootユーザーのパスワードをリセット
   mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PW}';"
-
+  
+  # 一般ユーザーを削除（存在する場合）
+  mysql -e "DROP USER IF EXISTS \`${MYSQL_USER_NAME}\`@'localhost';"
+  mysql -e "DROP USER IF EXISTS \`${MYSQL_USER_NAME}\`@'%';"
+  
   # 一般ユーザーを作成
-  mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER_NAME}\`@'localhost' IDENTIFIED BY '${MYSQL_USER_PW}';"
+  mysql -e "CREATE USER \`${MYSQL_USER_NAME}\`@'localhost' IDENTIFIED BY '${MYSQL_USER_PW}';"
   mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB_NAME}\`.* TO \`${MYSQL_USER_NAME}\`@'%';"
   mysql -e "FLUSH PRIVILEGES;"
 
